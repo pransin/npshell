f257e0b9d8e2e3e7fc751eeb105df06ec4db2484
Harsh-Butani <hbutani1808@gmail.com>
2022-10-12 23:48:46 +0530

    Implemented piping and execution of commands

--- shell.c
+++ shell.c
@@ -4,8 +4,14 @@
 #include <string.h>
 #include <ctype.h>
 #include <sys/types.h> //ssize_t
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <wait.h>
 
 #define DEFAULT_MALLOC_SIZE 4
+#define GREEN "\033[0;32m"
+#define RESET "\033[0;37m"
 
 enum ParseMode
 {
@@ -40,13 +46,170 @@ typedef struct Pipeline
 
 } Pipeline;
 
+int cnt_of_pipes;
+
+void error_exit(char *msg){
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void change_dir(char **args){
+    if(args[1] == NULL){
+        perror("Expected argument to \"cd\"\n");
+    }
+    else if(chdir(args[1]) != 0){
+        perror("chdir");
+    }
+}
+
+void free_pipeline(Pipeline *pipeline){
+    Command *tmp;
+    if(!pipeline){
+        return;
+    }
+    for(tmp=pipeline->cmd_list->next;tmp!=NULL;tmp=tmp->next){
+        free(tmp);
+    }
+    pipeline->last = pipeline->cmd_list;
+    pipeline->last->next = NULL;
+    pipeline->cnt = 0;
+}
+
+void close_all_pipes(int pipe_fd[][2], int count){
+    for(int i=0;i<count;i++){
+        close(pipe_fd[i][0]);
+        close(pipe_fd[i][1]);
+    }
+}
+
+bool is_valid_filename(char *filename){
+    int len = strlen(filename);
+    for(int i=0;i<len;i++){
+        if(isdigit(filename[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+void execute(Pipeline *pipeline, bool *exit){
+    if(!(pipeline->cnt)){
+        return;
+    }
+    if(!strcmp(pipeline->cmd_list->next->argv[0], "cd")){
+        change_dir(pipeline->cmd_list->next->argv);
+        return;
+    }
+    if(!strcmp(pipeline->cmd_list->next->argv[0], "exit")){
+        *exit = true;
+        return;
+    }
+    int count = pipeline->cnt;
+    int pipe_fd[cnt_of_pipes][2];
+    for(int i=0;i<cnt_of_pipes;i++){
+        if(pipe(pipe_fd[i]) == -1){
+            error_exit("pipe");
+        }
+    }
+    Command* cmd = pipeline->cmd_list->next;
+    int pipe_index = -1;
+    for(int i=0;i<count;i++){
+        if(cmd->out_count > 0){
+            pipe_index++;
+        }
+        pid_t ret = fork();
+        if(ret == -1){
+            error_exit("fork");
+        }
+        if(ret == 0){
+            if(cmd->out_count > 0){
+                if(pipe_index >= 1){
+                    if(dup2(pipe_fd[pipe_index-1][0], STDIN_FILENO) == -1){
+                        error_exit("dup2");
+                    }
+                    if(dup2(pipe_fd[pipe_index][1], STDOUT_FILENO) == -1){
+                        error_exit("dup2");
+                    }
+                }
+                else if(pipe_index == 0){
+                    if(dup2(pipe_fd[pipe_index][1], STDOUT_FILENO) == -1){
+                        error_exit("dup2");
+                    }
+                }
+            }
+            else{
+                if(pipe_index >= 0){
+                    if(dup2(pipe_fd[pipe_index][0], STDIN_FILENO) == -1){
+                        error_exit("dup2");
+                    }
+                }
+            }
+            if(cmd->input_redirect == true && is_valid_filename(cmd->input_file)){
+                int input_fd = open(cmd->input_file, O_RDONLY);
+                if(input_fd == -1){
+                    error_exit("open");
+                }
+                if(dup2(input_fd, STDIN_FILENO) == -1){
+                    error_exit("dup2");
+                }
+                if(close(input_fd) == -1){
+                    error_exit("close");
+                }
+            }
+            if(cmd->output_redirect == true && is_valid_filename(cmd->output_file)){
+                int output_fd = open(cmd->output_file, O_TRUNC | O_WRONLY | O_CREAT, 0777);
+                if(output_fd == -1){
+                    error_exit("open");
+                }
+                if(dup2(output_fd, STDOUT_FILENO) == -1){
+                    error_exit("dup2");
+                }
+                if(close(output_fd) == -1){
+                    error_exit("close");
+                }
+            }
+            if(cmd->output_append == true && is_valid_filename(cmd->output_file)){
+                int output_fd = open(cmd->output_file, O_APPEND | O_WRONLY | O_CREAT, 0777);
+                if(output_fd == -1){
+                    error_exit("open");
+                    return;
+                }
+                if(dup2(output_fd, STDOUT_FILENO) == -1){
+                    error_exit("dup2");
+                }
+                if(close(output_fd) == -1){
+                    error_exit("close");
+                }
+            }
+            close_all_pipes(pipe_fd, cnt_of_pipes);
+            if(execvp(cmd->argv[0], cmd->argv) == -1){
+                error_exit("execvp");
+            }
+        }
+        else{
+            if(pipe_index > 0){
+                close(pipe_fd[pipe_index-1][0]);
+            }
+            if(pipe_index < cnt_of_pipes){
+                close(pipe_fd[pipe_index][1]);
+            }
+            int status;
+            if(wait(&status) == -1){
+                error_exit("wait");
+            }
+            printf("Process[%d] pid: %d status: %d\n", i, ret, status);
+        }
+        cmd = cmd->next;
+    }
+    close_all_pipes(pipe_fd, cnt_of_pipes);
+}
+
 Command *create_cmd()
 {
     Command *cmd = (Command *)malloc(sizeof(Command));
     if (!cmd)
     {
-        perror("malloc");
-        return NULL;
+        error_exit("malloc");
     }
     cmd->argc = 0;
     cmd->argv = NULL;
@@ -74,7 +237,7 @@ void parse_cmd(Command *cmd, char *token)
         switch (token[i])
         {
         case '>':
-            if (token[i + 1] == '>')
+            if (i+1<len && token[i + 1] == '>')
             {
                 cmd->output_append = true;
                 token[i] = token[i + 1] = '\0';
@@ -147,13 +310,15 @@ char *read_cmds()
     char *commands = NULL;
     size_t size = 0;
     ssize_t result = getline(&commands, &size, stdin);
+    if(result == 1){
+        return NULL;
+    }
     if (result == -1)
     {
         free(commands);
         commands = NULL;
-        perror("Unable to read input");
+        error_exit("Unable to read input");
     }
-
     return commands;
 }
 
@@ -178,12 +343,18 @@ char *tokeniser(char **input, int *out_count)
     }
     else
     {
+        bool flag = 0;
         // skip continuous delims
         while (**input != '\0' && **input == '|')
         {
             **input = '\0';
             (*out_count)++;
             (*input)++;
+            flag = 1;
+        }
+
+        if(flag){
+            cnt_of_pipes++;
         }
 
         if (*out_count > 2)
@@ -199,6 +370,7 @@ void insert_cmd(Pipeline *pipeline, Command *cmd)
 {
     pipeline->last->next = cmd;
     pipeline->last = pipeline->last->next;
+    pipeline->last->next = NULL;
     pipeline->cnt++;
 }
 
@@ -219,25 +391,33 @@ void populate_pipeline(char *input, Pipeline *pipeline)
 Pipeline *create_pipeline(char *input)
 {
     Pipeline *pipeline = malloc(sizeof(Pipeline));
-    pipeline->cnt = 0;
-    pipeline->cmd_list = malloc(sizeof(Command));
-    pipeline->last = pipeline->cmd_list;
     if (!pipeline)
     {
-        perror("malloc");
-        return NULL;
+        error_exit("malloc");
     }
-
+    pipeline->cnt = 0;
+    pipeline->cmd_list = malloc(sizeof(Command));
+    if (!(pipeline->cmd_list))
+    {
+        error_exit("malloc");
+    }
+    pipeline->last = pipeline->cmd_list;
+    pipeline->last->next = NULL;
     populate_pipeline(input, pipeline);
     return pipeline;
 }
 
 int main()
 {
-    // while (1)
-    // {
-    printf("=> ");
-    char *input = read_cmds();
-    Pipeline *pipeline = create_pipeline(input);
-    // }
+    bool exit = false;
+    while (!exit)
+    {
+        printf(GREEN"=> "RESET);
+        char *input = read_cmds();
+        cnt_of_pipes = 0;
+        Pipeline *pipeline = create_pipeline(input);
+        execute(pipeline, &exit);
+        free_pipeline(pipeline);
+        printf("\n");
+    }
 }
